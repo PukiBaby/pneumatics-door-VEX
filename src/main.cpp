f3c3b532672a49695ceca7c720952cf5cde6576b
PukiBaby <pukibaby1234@gmail.com>
2024-12-06 09:47:36 +0800

    Add files via upload
    finished algorithm for door model
    features:
    > opens/closes a door if a distance sensor is activated; need to activate the other distance sensor so you can walk through
    > lock/unlock toggle button

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include "main.h"
 
+/*
+Why does 'A' have to be in apostrophes? 
+What is the difference between an apostrophe and a quotation mark?
+What are classes and objects; what exactly does :: do?
+What is typedef and enum?
+*/
+
+pros::ADIDigitalOut pneumatics('A');
+pros::Motor lock (4);
+
+pros::Controller master (pros::E_CONTROLLER_MASTER);
+
+pros::Distance distance_sensor_front (2);
+pros::Distance distance_sensor_back (3);
+
+void move_lock_wait(float distance){
+	lock.tare_position();
+	lock.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES); //degrees
+	lock.move_absolute(distance, 10); //10 RPM; presumably? distance in degrees
+	while ( !( lock.get_position() < distance + 5 && lock.get_position() > distance - 5 ) ){
+		pros::delay(20);
+	}
+	//pros::lcd::set_text(3, "lock movement complete");
+}
+
 void on_center_button() {
-	static bool pressed = false;
+	static bool pressed = false; //What is a static variable?
 	pressed = !pressed;
-	if (pressed) {
+	if(pressed) {
 		pros::lcd::set_text(2, "I was pressed!");
 	} else {
 		pros::lcd::clear_line(2);
@@ -12,7 +37,8 @@ void on_center_button() {
 
 void initialize() {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Program initialized.");
+	pros::lcd::set_text(1, "Code initialized");
+
 	pros::lcd::register_btn1_cb(on_center_button);
 }
 
@@ -23,46 +49,41 @@ void competition_initialize() {}
 void autonomous() {}
 
 void opcontrol() {
-	pros::lcd::set_text(2, "opcontrol initialized.");
-//Variables
-	int sensor_activated = 2;
-	/*
-	0 = front (port 1) was just activated
-	1 = back (port 2) was just activated
-	2 = none of them have been activated
-	*/
-	bool door_open = false;
-	int locked = 1; //1 is not locked; -1 is locked
-	float activation_distance = 1000.0; //mm
-	float lock_distance = 0.5; //turns
-//Sensors, output and controller
-	pros::adi::Pneumatics piston('A');
-	piston.set_value(false);
-	pros::Distance distance_front(1);
-	pros::Distance distance_back(2);
-	pros::Controller master (E_CONTROLLER_MASTER);
-	pros::Motor lock (3);
-//Loop
-	while(true){
-	//Prevent from using the same sensor to open and close the door AND open and close the door
-		if(distance_front.get() < activation_distance && sensor_activated != 0){
-			door_open = !door_open; //logical negation
-			piston.set_value(door_open);
-			sensor_activated = 0;
-		}
-		if(distance_back.get() < activation_distance && sensor_activated != 1){
-			door_open = !door_open; //logical negation
-			piston.set_value(door_open);
-			sensor_activated = 1;
-		}
-	//Lock and unlock the door
-		if(master.get_digital(E_CONTROLLER_DIGITAL_L1)){
-			lock.move_absolute(0, lock_distance*locked); // Moves 100 units forward or backward
-			while (!((lock.get_position() < lock_distance*locked + 0.1) && (lock.get_position() > lock_distance*locked - 0.1))) {
-				// Continue running this loop as long as the motor is not within +-0.1 turns of its goal
-				pros::delay(2);
+	//Variables
+		int door_unlocked = 1; //1 is true and -1 is false
+		int distance_sensor_last_used = 2; //0 is front, 1 is back, 2 is the initial state
+		bool door_open = false;
+	pros::lcd::set_text(2, "opcontrol initialized");
+	while (true){
+		//Lock AND unlock the door
+		if (master.get_digital(pros::E_CONTROLLER_DIGITAL_A) == 1){
+			//pros::lcd::set_text(3, "A was pressed");
+			move_lock_wait(-70.0 * door_unlocked);
+			door_unlocked = -1 * door_unlocked;
+			if (door_unlocked == 1){
+				pros::lcd::set_text(3, "door unlocked");
+			}
+			else
+			{
+				pros::lcd::set_text(3, "door locked");
 			}
 		}
-	pros::delay(20); //msec
+		if (distance_sensor_front.get() < 200 && !(distance_sensor_last_used == 0)){
+			distance_sensor_last_used = 0;
+			door_open = !door_open;
+			pneumatics.set_value(door_open);
+		}
+		if (distance_sensor_back.get() < 200 && !(distance_sensor_last_used == 1)){
+			distance_sensor_last_used = 1;
+			door_open = !door_open;
+			pneumatics.set_value(door_open);
+		}
+		if (door_open == true){
+			pros::lcd::set_text(4, "door open");
+		}
+		if (door_open == false){
+			pros::lcd::set_text(4, "door open");
+		}
+		pros::delay(20);
 	}
 }
